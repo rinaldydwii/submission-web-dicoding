4208fa6cfe06632dfee1cb22bf021970ef5a2460
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-12-31 07:31:56 +0700

    add collapse action to article title

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 console.log("Javascript started");
 
 window.onload = function(){
+    document.querySelectorAll(".collapseable")
+        .forEach(function(item) {
+            const title = item.querySelector('.article-title');
+            title.addEventListener('click', function() {
+                item.classList.toggle('collapsed')
+            })
+        })
 
 }
